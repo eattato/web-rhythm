6b9c17d1fb0b0840dbec367b8fb7b830df9d7459
eattato <eattato0804@gmail.com>
2023-03-07 12:45:29 +0900

    로딩 화면 추가, 파일 이동

--- pages/main/script.js
+++ pages/main/script.js
@@ -7,6 +7,21 @@ String.prototype.format = function () {
   };
 
 $().ready(() => {
+    let loading = $(".loading");
+    let loadingTick = 0
+    setInterval(() => {
+        loadingTick += 1
+        if (loadingTick > 3) {
+            loadingTick = 0
+        }
+
+        let ls = "로딩 중";
+        for (let ind = 1; ind <= loadingTick; ind++) {
+            ls += ".";
+        }
+        loading.find("div").text(ls);
+    }, 100);
+
     let record = $(".record");
     let recordRotation = 0;
     setInterval(() => {
@@ -16,5 +31,5 @@ $().ready(() => {
         }
 
         record.css({transform: "rotate({}deg)".format(recordRotation)});
-    }, 1)
+    }, 1);
 });
